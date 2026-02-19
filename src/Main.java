3429f9dd78968bff14a849eef987e0307a7f5f2b
AndresD <felipediazc08@gmail.com>
2026-02-19 13:42:48 -0500

    Proyecto 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.printf("============================================");
+        System.out.println("Andres Felipe Diaz Cortes");
+        System.out.println("Entrenar,Escuchar musica,Dibujar");
+        System.out.println("Levantamiendo de pesas");
+        System.out.println("===========================================");
     }
 }
