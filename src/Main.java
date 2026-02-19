33b095b32be752c97af94c93c4fd8279144b2ba4
SebasM <sebasmert.071203@gmail.com>
2026-02-19 13:54:53 -0500

    Proyecto 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Sebastian Alejandro Merchan Torres");
+        System.out.println("Mis 3 Hobbies jugar videojuegos, ver peliculas y ver series");
+        System.out.println("practico basquetball");
 
     }
 }
