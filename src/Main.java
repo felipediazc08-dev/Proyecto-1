93624bc915762dbd358bd799cb108a1acb57677b
ZARETHB <zarethoviedo1010@gmal.com>
2026-02-19 14:25:32 -0500

    Proyecto 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("Zareth Valentina Beltran Oviedo");
+        System.out.println("Tres hobbies escuchar musica ,ciclismo,leer");
+        System.out.println("Si,entrenar pesas");
     }
 }
